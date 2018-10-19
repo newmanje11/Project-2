5bd0d521e9a2449afee20703fc1bbedf93e45dfb
Jonathan Gaghan <jpgaghan@gmail.com>
2018-10-19 03:41:10 -0400

    times giphy spotify ticketmaster

--- public/js/ticketmaster.js
+++ public/js/ticketmaster.js
@@ -3,15 +3,15 @@ $(document).on("click", ".concerts", function () {
     console.log("I've been clicked");
 
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Fst7jzMSw05CNr3UdA1wrZAywnNi0A3j";
-
-    var startDate = "2018-10-01T01:00:00Z";
-    var endDate = "2019-12-31T21:59:00Z";
-    var limit = 10;
     var currEle = $(this)
+    var startDate = currEle[0].childNodes[5].dataset.sdate;
+    var endDate = currEle[0].childNodes[5].dataset.edate;
+    var limit = 10;
+    console.log(currEle)
     var city = currEle[0].childNodes[1].innerText;
     // console.log(city);
 
-    
+
     $.ajax({
         url: queryURL,
         method: "GET",
