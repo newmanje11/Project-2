e78e6ef5567dee90ca8678ec8bcb83c15025852d
Jonathan Gaghan <jpgaghan@gmail.com>
2018-10-19 22:57:42 -0400

    models

--- public/js/ticketmaster.js
+++ public/js/ticketmaster.js
@@ -1,22 +1,23 @@
 $(document).on("click", ".concerts", function () {
     
-    window.location.href = "/events"
+    // window.location.href = "/events"
     event.preventDefault();
     console.log("I've been clicked");
 
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Fst7jzMSw05CNr3UdA1wrZAywnNi0A3j";
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Z5KkKdg29zBxIkQFJCnXrG3TnUXuj1YW";
     var currEle = $(this)
     var location = currEle[0].childNodes[1].innerText;
     var state = location.slice(-2)
     console.log(state)
-    $.post("/state",{sloc:state})
+    $.post("/state",state)
     console.log(currEle)
     var startDate = currEle[0].childNodes[5].dataset.sdate;
+    console.log(startDate)
     var endDate = currEle[0].childNodes[5].dataset.edate;
+    console.log(endDate)
     var limit = 10;
     console.log(currEle)
-    var city = location.slice(0, -4)
-    console.log(city)
+    var city = currEle[0].childNodes[1].innerText;
     // console.log(city);
 
 
@@ -27,7 +28,7 @@ $(document).on("click", ".concerts", function () {
             city: city,
             startDateTime: startDate,
             endDateTime: endDate,
-            size: limit,
+            // size: limit,
             locale: "en"
         }
     }).then(function (response) {
@@ -36,7 +37,7 @@ $(document).on("click", ".concerts", function () {
 
         eventArray = []
         for (i = 0; i < 10; i++) {
-
+console.log(response._embedded)
             eventDates = response._embedded.events[i].dates.start.localDate;
             eventTime = response._embedded.events[i].dates.start.localTime;
             eventPics = response._embedded.events[i].images[0].url;
